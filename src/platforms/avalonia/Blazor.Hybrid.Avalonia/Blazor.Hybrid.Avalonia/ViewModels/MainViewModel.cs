fd8b708502a75030a6716f9ef6041bb6d42dc12f
lytree <hi.yangzh@outlook.com>
2024-10-11 09:32:37 +0800

    update

--- src/platforms/avalonia/Blazor.Hybrid.Avalonia/Blazor.Hybrid.Avalonia/ViewModels/MainViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blazor.Hybrid.Avalonia.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
-}
--- src/platforms/avalonia/Blazor.Hybrid.Avalonia/Blazor.Hybrid.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,6 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Blazor.Hybrid.Avalonia.ViewModels;
-public class ViewModelBase : ReactiveObject
-{
-}
